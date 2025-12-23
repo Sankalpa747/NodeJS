cdb1e4d361985e8b3edc9bb0a586d664e1d1a3e3
UWIJES1 <sankalpa.wijewickrama@pearson.com>
2025-12-23 19:26:10 +0530

    ejs

---
+++ nodejs_complete_guide/archives/5_introduce_template_engine_ejs/routes/admin.js
@@ -0,0 +1,43 @@
+// Importing express module (To create a router)
+const express = require("express");
+// Importing path module (To join the current directory with the views directory and the add-product.html file)
+const path = require("path");
+
+// Importing path module (To get the directory name of the current file)
+const rootDir = require("../util/path");
+
+// Create a router
+const router = express.Router();
+
+const products = [];
+
+// Middleware function for the /add-product route
+router.get("/add-product", (req, res, next) => {
+    console.log("admin.js");
+    // Join the current directory with the views directory and the add-product.html file
+    // rootDir - The directory name of the current file
+    // 'views' - The directory name
+    // 'add-product.html' - The file name
+    //res.sendFile(path.join(rootDir, "views", "add-product.html"));
+
+    // Render the add-product.pug file
+    res.render("add-product", { 
+        docTitle: "Add Product", 
+        path: "/admin/add-product",
+        activeAddProduct: true,
+        productCSS: true,
+        formCSS: true,
+     });
+});
+
+// Middleware function for the /add-product route
+router.post("/add-product", (req, res, next) => {
+    products.push({title: req.body.title});
+
+    // Redirect to the / route
+    res.redirect("/");
+});
+
+// Export the router and the products array
+exports.router = router;
+exports.products = products;
---
+++ nodejs_complete_guide/archives/5_introduce_template_engine_ejs/routes/shop.js
@@ -0,0 +1,39 @@
+// Importing express module (To create a router)
+const express = require("express");
+// Importing path module (To join the current directory with the views directory and the shop.html file)
+const path = require("path");
+
+// Importing path module (To get the directory name of the current file)
+const rootDir = require("../util/path");
+
+// Importing the admin data
+const adminData = require("./admin");
+
+// Create a router
+const router = express.Router();
+
+// Middleware function for the / route
+router.get("/", (req, res, next) => {
+    console.log(adminData.products);
+
+    // Join the current directory with the views directory and the shop.html file
+    // rootDir - The directory name of the current file
+    // 'views' - The directory name
+    // 'shop.html' - The file name
+    //res.sendFile(path.join(rootDir, "views", "shop.html"));
+
+    const products = adminData.products;
+
+    // Render the shop.pug file
+    res.render("shop", { 
+        prods: products, 
+        docTitle: "Shop", 
+        path: "/", 
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+     });
+});
+
+// Export the router
+module.exports = router;
