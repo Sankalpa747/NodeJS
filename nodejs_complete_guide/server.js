7176d9d08d4e0ec7ec924b9b9119f8de9d01265f
UWIJES1 <sankalpa.wijewickrama@pearson.com>
2025-12-24 13:31:20 +0530

    Controller improvements

--- nodejs_complete_guide/server.js
+++ nodejs_complete_guide/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+// Importing the 404 controller
+const errorController = require("./controllers/error");
+
 // Create an express application
 const app = express();
 
@@ -34,10 +37,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 // 404 middleware
-app.use((req, res, next) => {
-    // Render the page-not-found.pug file
-    res.status(404).render("page-not-found", { docTitle: "Page Not Found", path: "/404" });
-});
+app.use(errorController.get404);
 
 // Listen to the port 3000
 app.listen(3000);
